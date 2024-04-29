5cfe83a156789d58c69c48146b8d8cf29f8f4bae
aspenos <arisspenos1@gmail.com>
2024-04-29 13:29:39 -0400

    Pre-save hook to hash password before saving a user document

--- models/userModel.js
+++ models/userModel.js
@@ -21,3 +21,14 @@ const userSchema = new mongoose.Schema({
         ref: 'Divvy'
     }]
 });
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
