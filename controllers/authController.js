9e8d31e81663c0744c861e0724d393cade6fcbdb
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-05-01 12:02:46 -0400

    fix copy-pasta auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,13 @@ import User from '../models/userModel.js';
 
 export const signup = async (req, res) => {
     try {
-        req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10));
+        bcrypt.hash(req.body.password, 10, function(err, hash) {
+            if (err) { throw (err); }
+            bcrypt.compare(req.body.password, hash, function(err, result) {
+                if (err) { throw (err); }
+                req.body.password = hash;
+            });
+        });
         const user = await User.create(req.body);
         res.json(user);
     } catch (error) {
@@ -13,22 +19,15 @@ export const signup = async (req, res) => {
     }
 };
 
-/**
- * Handles user login.
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- * @returns {Promise<void>} - A promise that resolves when the login process is complete.
- */
 export const login = async (req, res) => {
     try {
-        const user = await User.findOne({ name: req.body.name }).populate('closetItems').populate('associatedTags');
+        const user = await User.findOne({ email: req.body.email })
         if (user) {
             const result = await bcrypt.compare(req.body.password, user.password);
-    
             if (result) {
-                const payload = { name: user.name };
-                const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '1d' }); // Example: Expiring in 1 day
-                res.cookie('token', token, { httpOnly: true }).json({ user: user });
+                const payload = { id: user._id, email: user.email, displayName: user.displayName };
+                const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '1d' });
+                res.cookie('token', token, { httpOnly: true }).json(user);
             } else {
                 res.status(400).json({ error: 'Password does not match' });
             }
@@ -39,6 +38,7 @@ export const login = async (req, res) => {
         res.status(500).json({ error: error.message })
     }
 };
+
 
 export const logout = async (req, res) => { 
     res.clearCookie('token').json({ message: 'Logout successful' });
