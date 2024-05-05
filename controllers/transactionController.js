d017e8ac7b933f1215a0f1693e0916815277a2cd
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-05-05 13:11:43 -0400

    let the good times roll

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -19,7 +19,7 @@ export const createTransaction = async (req, res) => {
     if (!divvy) {
       return res.status(404).json({ message: 'Divvy not found' });
     }
-
+    
     // Create an array for each person involved in the transaction
     const promisedWork = breakdown.map(async personOfInterest => {
       try {
@@ -98,7 +98,7 @@ try {
 
   //update the transaction
   transaction.transactionName = transactionName ? transactionName : transaction.transactionName;
-  transaction.amount = typeof amount === 'number' ? amount : transaction.amount;
+  transaction.amount = amount ? amount : transaction.amount;
   transaction.paidBy = paidBy ? paidBy : transaction.paidBy;
   transaction.type = type ? type : transaction.type;
   transaction.breakdown = breakdown ? breakdown : transaction.breakdown;
@@ -106,6 +106,7 @@ try {
   //save to the divvy
   divvy.transactions.id(transactionId).set(transaction);
   //reapply the breakdown to the participants
+  console.log(breakdown)
   // Create an array for each person involved in the transaction
   const promisedWork = breakdown.map( async personOfInterest => {
     try {
@@ -135,23 +136,27 @@ try {
 
   // Wait for all the promises to resolve
   const updated = await Promise.all(promisedWork);
-  
   //find the participant in the divvy who wern't in the breakdown
   const participantsNotInBreakdown = divvy.participants.filter(participant => {
     return !updated.includes(participant);
   });
-
+  console.log('participants Not in Breakdown',participantsNotInBreakdown)
   //concat the participants not in the breakdown to the updated participants
   const finalParticipantArray = updated.concat(participantsNotInBreakdown);
 
+  console.log('finalParticipantArray',finalParticipantArray)
   //update the divvy with the final participant array
   divvy.participants = finalParticipantArray;
 
+  console.log('divvy.participants',divvy.participants)
+  //save the divvy
+  await divvy.save();
   //get user of divvy
   const user = await User.findById(divvy.owner);
   const populated = await user.populate('Divvys');
   const { _id, email, displayName, Divvys} = populated;
   const response = { userID: _id, email, displayName, Divvys };
+  console.log('response',response.Divvys[0].participants)
   res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ message: 'Error updating transaction', error: error.message });
