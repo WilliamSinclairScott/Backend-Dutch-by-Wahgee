6dded77a002f17255a3631d13009730b4e1524a2
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-05-06 03:52:57 -0400

    bugfix

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const transactionSchema = new mongoose.Schema({
   transactionName: {
     type: String,
-    unique : true,
+    //unique : true,
     required: true
   },
   date: {
